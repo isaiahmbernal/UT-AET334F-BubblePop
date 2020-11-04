44425511450bb397aa1f660453dace6830af298d
isaiahmbernal <39536392+isaiahmbernal@users.noreply.github.com>
2020-11-03 23:08:30 -0600

    Working Health

--- AET 334F - Group Project/Assets/Scripts/Gameplay_DestroyNotes.cs
+++ AET 334F - Group Project/Assets/Scripts/Gameplay_DestroyNotes.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Gameplay_DestroyNotes : MonoBehaviour
 {
+    public Input_Gameplay currentHealth;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         // Debug.Log("Destroyed Note");
         if (other.gameObject.tag == "Note")
+        {
+            currentHealth.health--;
             Destroy(other.gameObject);
+        }
     }
 }
