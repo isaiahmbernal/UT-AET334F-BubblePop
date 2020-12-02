a75100cc2dd2229cebc69c97bb43710467d46d9a
bsw838 <63919382+bsw838@users.noreply.github.com>
2020-12-02 10:24:46 -0600

    DialogueManager

---
+++ AET 334F - Group Project/Assets/Scripts/DialogueScripts/DialogueTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+
+    public Dialogue reply1;
+
+    public Dialogue reply2;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            TriggerDialogue();
+        }
+    }
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueTree>().StartDialogue(dialogue, reply1, reply2);
+    }
+}
